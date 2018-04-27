05de1569ccae5bbd33642b30b06bad1977be4fe9
yuguansu <yuguansu1990@gmail.com>
2018-04-27 20:55:39 +0800

    180427 20.55

---
+++ DAL.FileHelper2/FileHelper/CSVHelper.cs
@@ -0,0 +1,217 @@
+﻿using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DAL.FileHelper
+{
+    public class CSVHelper
+    {
+        /// <summary>
+<<<<<<< HEAD
+        /// 将datatable数据全部写入csv中，按逗号分割，列标题作为第一行
+=======
+        /// 将datatable数据全部写入csv中，按逗号分割
+>>>>>>> f8d4f85... 1.0.0.1 删除CSV写入时，弹出MessageBox。 增加CSV写入字符串。
+        /// </summary>
+        /// <param name="dt">datatable数据源</param>
+        /// <param name="fullPath">需要写入的csv文件的完整路径</param>
+        public static void WriteCSV(DataTable dt,string fullPath)
+        {
+            FileInfo file = new FileInfo(fullPath);
+            if (!file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+            FileStream fs = new FileStream(fullPath, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            string data = "";
+            //写列名
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                data += dt.Columns[i].ColumnName.ToString() +",";
+            }
+            sw.WriteLine(data);
+            //写数据
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                data = "";
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    string str = dt.Rows[i][j].ToString();
+                    str = str.Replace("\"", "\"\"");
+                    if (str.Contains(',')||str.Contains('"')||str.Contains('\r')||str.Contains('\n'))
+                    {
+                        str = string.Format("\"{0}\"",str);
+                    }
+                    data += str + ",";
+                }
+                sw.WriteLine(data);
+            }
+            sw.Close();
+            fs.Close();
+<<<<<<< HEAD
+=======
+            //DialogResult result = MessageBox.Show("CSV文件保存成功");
+            //if (result == DialogResult.OK)
+            //{
+            //    System.Diagnostics.Process.Start("explorer.exe");
+            //}
+        }
+        /// <summary>
+        /// 写入一行字符串
+        /// </summary>
+        /// <param name="sLogInfo">写入信息</param>
+        /// <param name="fullPath">csv文件的完整路径</param>
+        public static void WriteCSV(string sLogInfo, string fullPath)
+        {
+            FileInfo file = new FileInfo(fullPath);
+            if (!file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+            FileStream fs = new FileStream(fullPath, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            
+            sw.WriteLine(sLogInfo);
+            
+            sw.Close();
+            fs.Close();
+            //DialogResult result = MessageBox.Show("CSV文件保存成功");
+            //if (result == DialogResult.OK)
+            //{
+            //    System.Diagnostics.Process.Start("explorer.exe");
+            //}
+>>>>>>> f8d4f85... 1.0.0.1 删除CSV写入时，弹出MessageBox。 增加CSV写入字符串。
+        }
+        
+        /// <summary>
+        /// 读取csv文件中所有数据到datatable中，第一行为列标题，其余为数据，列数以标题为主，按逗号分割
+        /// </summary>
+        /// <param name="csvPath">csv文件的完整路径</param>
+        /// <returns>返回包含csv数据的datatable，如果错误则返回Null</returns>
+        public static DataTable ReadCSV(string csvPath)
+        {
+            if (!File.Exists(csvPath.Trim())) return null;
+
+            DataTable dt = new DataTable();
+            bool isFirst = true;
+
+            var lines = File.ReadAllLines(csvPath);
+
+            foreach (var line in lines)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                    string[] tableHeader = line.Split(',');
+                    foreach (var header in tableHeader)
+                    {
+                        dt.Columns.Add(header.ToString());
+                    }
+                }
+                else
+                {
+                    string[] data = line.Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        dr[i] = data[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+        /// <summary>
+<<<<<<< HEAD
+        /// 读取csv文件中所有数据到datatable中，第一行为列标题，其余为数据，列数以标题为主，按指定分隔符分割
+        /// </summary>
+        /// <param name="csvPath">csv文件的完整路径</param>
+        /// <returns>返回包含csv数据的datatable，如果错误则返回Null</returns>
+        public static DataTable ReadCSV(string csvPath,char[] splitChars)
+        {
+            if (!File.Exists(csvPath.Trim())) return null;
+
+            DataTable dt = new DataTable();
+            bool isFirst = true;
+
+            var lines = File.ReadAllLines(csvPath);
+
+            foreach (var line in lines)
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                    string[] tableHeader = line.Split(splitChars);
+                    foreach (var header in tableHeader)
+                    {
+                        dt.Columns.Add(header.ToString());
+                    }
+                }
+                else
+                {
+                    string[] data = line.Split(splitChars);
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        dr[i] = data[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+        /// <summary>
+=======
+>>>>>>> f8d4f85... 1.0.0.1 删除CSV写入时，弹出MessageBox。 增加CSV写入字符串。
+        /// 读取csv第一行数据，按逗号分割为string数组
+        /// </summary>
+        /// <param name="csvPath">csv文件的完整路径</param>
+        /// <returns></returns>
+        public static string[] ReadFirstLine(string csvPath)
+        {
+            if (!File.Exists(csvPath.Trim())) return null;
+            var line = File.ReadAllLines(csvPath).First();
+            return line.Split(',');
+        }
+        /// <summary>
+<<<<<<< HEAD
+        /// 读取csv第一行数据，按指定分隔符分割为string数组
+        /// </summary>
+        /// <param name="csvPath">csv文件的完整路径</param>
+        /// <param name="chrSplit">分隔符</param>
+        /// <returns></returns>
+        public static string[] ReadFirstLine(string csvPath,char chrSplit)
+        {
+            if (!File.Exists(csvPath.Trim())) return null;
+            var line = File.ReadAllLines(csvPath).First();
+            return line.Split(chrSplit);
+        }
+        /// <summary>
+        /// 读取csv第一行数据，按多个指定分隔符分割为string数组
+        /// </summary>
+        /// <param name="csvPath">csv文件的完整路径</param>
+        /// <param name="chrSplit">分隔符列表</param>
+        /// <returns></returns>
+        public static string[] ReadFirstLine(string csvPath, char[] chrSplit)
+        {
+            if (!File.Exists(csvPath.Trim())) return null;
+            var line = File.ReadAllLines(csvPath).First();
+            return line.Split(chrSplit);
+        }
+        /// <summary>
+        /// 读取csv第一行数据
+=======
+        /// 读取csv第一行数据，按逗号分割为string数组
+>>>>>>> f8d4f85... 1.0.0.1 删除CSV写入时，弹出MessageBox。 增加CSV写入字符串。
+        /// </summary>
+        /// <param name="csvPath">csv文件的完整路径</param>
+        /// <returns></returns>
+        public static string ReadFirstLineString(string csvPath)
+        {
+            if (!File.Exists(csvPath.Trim())) return null;
+            return File.ReadAllLines(csvPath).First();
+        }
+    }
+}
---
+++ DAL.FileHelper2/FileHelper/IniHelper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DAL.FileHelper
+{
+    public class IniHelper
+    {
+        private string iniFileName;
+
+        public string FileName
+        {
+            get { return iniFileName; }
+            set { iniFileName = value; }
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileInt(
+            string lpAppName,
+            string lpKeyName,
+            int nDefault,
+            string lpFileName
+            );
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(
+            string lpAppName,
+            string lpKeyName,
+            string lpDefault,
+            StringBuilder lpReturnedString,
+            int nSize,
+            string lpFileName
+            );
+
+        [DllImport("kernel32.dll")]
+        private static extern int WritePrivateProfileString(
+            string lpAppName,
+            string lpKeyName,
+            string lpString,
+            string lpFileName
+            );
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="aFileName">Ini文件路径</param>
+        public IniHelper(string aFileName)
+        {
+            this.iniFileName = aFileName;
+        }
+        public IniHelper()
+        {
+
+        }
+
+
+        /// <summary>
+        /// [扩展]读Int数值
+        /// </summary>
+        /// <param name="section">节</param>
+        /// <param name="name">键</param>
+        /// <param name="def">默认值</param>
+        /// <returns></returns>
+        public int ReadInt(string section, string name, int def)
+        {
+            return GetPrivateProfileInt(section, name, def, this.iniFileName);
+        }
+
+        /// <summary>
+        /// [扩展]读取string字符串
+        /// </summary>
+        /// <param name="section">节</param>
+        /// <param name="name">键</param>
+        /// <param name="def">默认值</param>
+        /// <returns></returns>
+        public string ReadString(string section, string name, string def)
+        {
+            StringBuilder vRetSb = new StringBuilder(2048);
+            GetPrivateProfileString(section, name, def, vRetSb, 2048, this.iniFileName);
+            return vRetSb.ToString();
+        }
+
+        /// <summary>
+        /// [扩展]写入Int数值，如果不存在 节-键，则会自动创建
+        /// </summary>
+        /// <param name="section">节</param>
+        /// <param name="name">键</param>
+        /// <param name="Ival">写入值</param>
+        public void WriteInt(string section, string name, int Ival)
+        {
+            WritePrivateProfileString(section, name, Ival.ToString(), this.iniFileName);
+        }
+
+        /// <summary>
+        /// [扩展]写入String字符串，如果不存在 节-键，则会自动创建
+        /// </summary>
+        /// <param name="section">节</param>
+        /// <param name="name">键</param>
+        /// <param name="strVal">写入值</param>
+        public void WriteString(string section, string name, string value)
+        {
+            WritePrivateProfileString(section, name, value, this.iniFileName);
+        }
+
+        /// <summary>
+        /// 删除指定的 节
+        /// </summary>
+        /// <param name="section"></param>
+        public void DeleteSection(string section)
+        {
+            WritePrivateProfileString(section, null, null, this.iniFileName);
+        }
+
+        /// <summary>
+        /// 删除全部 节
+        /// </summary>
+        public void DeleteAllSection()
+        {
+            WritePrivateProfileString(null, null, null, this.iniFileName);
+        }
+
+    }
+}
